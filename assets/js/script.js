6091526d37333a7aaae47ae650d2d27b3643fe50
vlsulliv <vlsulliv@yahoo.com>
2022-01-09 10:42:44 -0600

    Add moment and styling for mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,67 @@
-let fontFace = new FontFace('Roboto', 'url(https://fonts.example.com/roboto.woff2)');
+
+let fontFace = new FontFace(
+  'Roboto',
+  'url(https://fonts.example.com/roboto.woff2)',
+);
 console.log(fontFace.family); // 'Roboto'
 
 fontFace.family = 'newRoboto';
 console.log(fontFace.family); // 'newRoboto'
 
-let P1URL = document.getElementById('note-taker')
-P1URL.addEventListener('click', function() {
-    var window = window.open("https://vlsulliv.github.io/note-taker/", "_blank")
-})
+let P1URL = document.getElementById('note-taker');
+P1URL.addEventListener('click', function () {
+  var window = window.open('https://vlsulliv.github.io/note-taker/', '_blank');
+});
+
+console.log('');
+console.log('------------------');
+console.log('');
+console.log(moment().format('L'));
+console.log(moment().format('LL'));
+console.log(moment().format('LLL'));
+console.log(moment().format('LLLL'));
+
+console.log('');
+console.log('------------------');
+console.log('');
+
+console.log(moment().format('dddd'));
+console.log(moment().format('Y'));
+console.log(moment().format('LT'));
+console.log(moment().format('l'));
+console.log(moment().format('lll'));
+
+console.log('');
+console.log('------------------');
+console.log('');
+
+console.log(moment().format('Z'));
+console.log(moment().format('Q'));
+console.log(moment().format('MMM'));
+console.log(moment().format('gggg'));
+console.log(moment().format('GG'));
+
+console.log("");
+console.log("------------------");
+console.log('');
+
+console.log(moment().format('LTS'));
+console.log(moment().format('MMM'));
+console.log(moment().format('HHMM')); //military time
+console.log(moment().format('YYYY-MM-DD')); 
+
+console.log('');
+console.log('------------------');
+console.log('');
+
+console.log(moment().format('[Today at] LT')); 
+console.log(moment().format('[Yesterday at] LT')); 
+console.log(moment().format('[Tomorrow at] LT')); 
+
+console.log('');
+console.log('------------------');
+console.log('');
+
+// var day = moment(1318781876406);
+// console.log(day)
+
