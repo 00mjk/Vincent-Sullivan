28ad66bea62876c90bd4181c5fa6308a3cc7cca8
vlsulliv <vlsulliv@yahoo.com>
2022-02-04 18:44:05 -0600

    attempt 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,33 @@
-// const prismjs = require('prismjs');
 
 
-let fontFace = new FontFace(
-  'Roboto',
-  'url(https://fonts.example.com/roboto.woff2)',
-);
-// console.log(fontFace.family); // 'Roboto'
 
-// fontFace.family = 'newRoboto';
-// console.log(fontFace.family); // 'newRoboto'
+function speech() {
+if ('speechSynthesis' in window) {
+	var msg = new SpeechSynthesisUtterance();
+	msg.text = "Good Morning";
+	window.speechSynthesis.speak(msg);
+} else {
+	 alert("Sorry, your browser doesn't support text to speech!");
+	}
+}
 
-// let P1URL = document.getElementById('note-taker');
-// P1URL.addEventListener('click', function () {
-//   var window = window.open('https://vlsulliv.github.io/note-taker/', '_blank');
-// });
 
-console.log('');
-console.log('------------------');
-console.log('');
-console.log(moment().format('L'));
-console.log(moment().format('LL'));
-console.log(moment().format('LLL'));
-console.log(moment().format('LLLL'));
 
-console.log('');
-console.log('------------------');
-console.log('');
 
-console.log(moment().format('dddd'));
-console.log(moment().format('Y'));
-console.log(moment().format('LT'));
-console.log(moment().format('l'));
-console.log(moment().format('lll'));
 
-console.log('');
-console.log('------------------');
-console.log('');
 
-console.log(moment().format('Z'));
-console.log(moment().format('Q'));
-console.log(moment().format('MMM'));
-console.log(moment().format('gggg'));
-console.log(moment().format('GG'));
+const FToC = through2.ctor({objectMode: true}, function (record, encoding, callback) {
+  if (record.temp != null && record.unit == "F") {
+    record.temp = ( ( record.temp - 32 ) * 5 ) / 9
+    record.unit = "C"
+  }
+  this.push(record)
+  callback()
+})
 
-console.log("");
-console.log("------------------");
-console.log('');
-
-console.log(moment().format('LTS'));
-console.log(moment().format('MMM'));
-console.log(moment().format('HHMM')); //military time
-console.log(moment().format('YYYY-MM-DD')); 
-
-console.log('');
-console.log('------------------');
-console.log('');
-
-console.log(moment().format('[Today at] LT')); 
-console.log(moment().format('[Yesterday at] LT')); 
-console.log(moment().format('[Tomorrow at] LT')); 
-
-console.log('');
-console.log('------------------');
-console.log('');
-
-// var day = moment(1318781876406);
-// console.log(day)
-
+// Create instances of FToC like so:
+const converter = new FToC()
+// Or:
+const converter = FToC()
+// Or specify/override options when you instantiate, if you prefer:
+const converter = FToC({objectMode: true})
