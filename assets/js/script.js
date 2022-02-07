91e280d1fc5955eee5af9ce5915b2ece52b1aa2b
vlsulliv <vlsulliv@yahoo.com>
2022-02-07 00:15:06 -0600

    fix desktop layout for profile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
+let currentDate = new Date()
 
-
+console.log(currentDate);
 
 function speech() {
 if ('speechSynthesis' in window) {
@@ -10,24 +11,3 @@ if ('speechSynthesis' in window) {
 	 alert("Sorry, your browser doesn't support text to speech!");
 	}
 }
-
-
-
-
-
-
-const FToC = through2.ctor({objectMode: true}, function (record, encoding, callback) {
-  if (record.temp != null && record.unit == "F") {
-    record.temp = ( ( record.temp - 32 ) * 5 ) / 9
-    record.unit = "C"
-  }
-  this.push(record)
-  callback()
-})
-
-// Create instances of FToC like so:
-const converter = new FToC()
-// Or:
-const converter = FToC()
-// Or specify/override options when you instantiate, if you prefer:
-const converter = FToC({objectMode: true})
